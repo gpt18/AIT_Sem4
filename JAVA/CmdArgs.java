a6206a1417cc0f15018b58ba008f7dbfc546520f
gpt18 <gudduprajapati626@gmail.com>
2021-03-04 21:48:26 +0530

    main

---
+++ JAVA/CmdArgs.java
@@ -0,0 +1,12 @@
+class CmdArgs {
+    //Command Line arguments in Java
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("This command line"+" arguments are:");
+            for(String val:args)
+                System.out.println(val);
+        } else {
+            System.out.println("No command line "+"argumaents found.");
+        }
+    }
+}
---
+++ JAVA/First.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+import javax.print.event.PrintJobListener;
+
+class First{
+    public static void main(String[] args) {
+        //this is single line comment
+        /*this is 
+        multi line
+        comment */
+        System.out.println("This is my first java program");
+        System.out.println("This is output second line");
+        System.out.print("This is output third line");
+    }/**documentation comment also called doc comment */
+}
